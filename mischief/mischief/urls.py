cde5fe905cf7d12c3b96ec7eef40804dcc69fc7f
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-10 07:27:31 +0100

    Create simple IndexView to check routing

--- mischief/mischief/urls.py
+++ mischief/mischief/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('core.urls')), 
-    path('user', include('users.urls')), 
+    #path('user', include('users.urls')), 
     path('nibble', include('nibble.urls')), 
     path('squeak', include('squeak.urls')), 
 ]
